7c6cc0419adf6d371327201711bdc03aa42254a6
Emre DURMAZ <emredrmz89@gmail.com>
2021-05-09 22:25:18 +0300

    Nero STARBUCKS

--- homeWorkDay4Work2/src/Main.java
+++ homeWorkDay4Work2/src/Main.java
@@ -10,15 +10,9 @@ import entities.Customer;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
-			customerManager.add(new Customer(1,"emre","durmaz",LocalDate.of(1989, 04, 20),"23645108518"));
-			
-			
-		
-		
-		
-		
+		customerManager.add(new Customer(1, "emre", "durmaz", LocalDate.of(1989, 04, 20), "23645108518"));
 
 	}
 
